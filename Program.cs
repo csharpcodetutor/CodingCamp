54f698fffec0276e90b357c3df256775739c0b8f
DeeProgrammer <deeprogrammer@gmail.com>
2021-06-11 02:40:01 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingCamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
